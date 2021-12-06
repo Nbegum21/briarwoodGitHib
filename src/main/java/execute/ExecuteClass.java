9866491ab0591f66f82fbdda89019b68c1e558af
Nur <nbegum.realtor@gmail.com>
2021-12-06 11:42:58 -0500

    Added new line in the main method

--- src/main/java/execute/ExecuteClass.java
+++ src/main/java/execute/ExecuteClass.java
@@ -2,6 +2,9 @@ package execute;
 
 public class ExecuteClass {
     public static void main(String[] args) {
+
         System.out.println("hello");
+        System.out.println("My Name is Nur");
     }
+    
 }
