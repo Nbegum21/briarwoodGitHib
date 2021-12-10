465f0bcea7ab59e423f2f450277f59510530e2f2
Nur <nbegum.realtor@gmail.com>
2021-12-10 11:35:49 -0500

    updated testng.xml

--- src/main/java/execute/ExecuteClass.java
+++ src/main/java/execute/ExecuteClass.java
@@ -1,10 +1,11 @@
 package execute;
 
 public class ExecuteClass {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("My Name is Nur");
+        System.out.println("this is new project");
     }
-    
+
 }
